1cd8d1e2b234c039894a8ba151d7da50550ad927
Viktor Pronin <exppro1@gmail.com>
2020-07-01 23:33:53 +0300

    add todos hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+
+interface ITodo {
+  index: number,
+  text: string,
+  completed: boolean
+}
+
+const TodoApp: React.FC = () => {
+  // const todos: ITodo[] = [];
+  const [todos, setTodo] = useState<ITodo[]>([])
+
+  let textValue: string = '';
+
+  const addTodo = (text: string) => {
+    let newTodo = { index: Date.now(), text: text, completed: false }
+    setTodo(todos => [...todos, newTodo]);
+  }
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    // сохраняем значение поля
+    textValue = (event.target as HTMLInputElement).value;
+
+    // если ентер
+    if (event.keyCode === 13) {
+      //  - выполняем хук
+      addTodo(textValue);
+      // делаем reset для поля ввода
+      (event.target as HTMLInputElement).value = '';
+    }
+
+  }
+
+  return (
+    <div className="layout">
+      {todos.map(i =>
+        <div key={i.index}>{i.text}</div>
+      )}
+      <div className="addTodo">
+        <div className="addTodo__wrapper">
+          <div className="addTodo__row container">
+            <div >
+              <input type="text" name="textInput" className="textInput"
+                onKeyDown={handleKeyDown} placeholder="Add new todo here then hit enter"></input>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+    <TodoApp />
+  </>
   );
 }
 
